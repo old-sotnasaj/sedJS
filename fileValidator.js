5b322ccf854687512ebcb2808d2544a1278c4768
Javier Santos <sbjaviersantos@gmail.com>
2020-05-06 07:11:55 -0600

    checking [files] exist/readable

--- fileValidator.js
+++ fileValidator.js
@@ -1,5 +1,21 @@
-module.exports = {};
+const fs = require('fs');
 
-function validatePath(filePath) {
-  if (filePath !== undefined) return true;
+// Sample data to make faster checks, it will be eventuallly removed
+// const testFilePath = ['../empty.txt', '../other.txt'];
+
+function checkReadExist(files) {
+  for (let file of files) {
+    access(file, constants.F_OK | constants.R_OK, (err) => {
+      if (err) {
+        let errorMsg;
+        errorMsg = err.code === 'ENOENT' ? 'does not exist' : 'is not readable';
+        console.error(file + ' ' + errorMsg);
+        return false;
+      }
+    });
+  }
+  // If we get up to here it means that there are no errors , so we retrieve true
+  return true;
 }
+
+module.exports =  { checkReadExist };
