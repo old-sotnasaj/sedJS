8ac367268aa57c10645ae041b03b355b19a5dd5c
Javier Santos <sbjaviersantos@gmail.com>
2020-05-07 01:58:53 -0600

    checking only one command

--- regexValidator.js
+++ regexValidator.js
@@ -1,5 +1,39 @@
-module.exports = {};
+const stringTest = 's/poko/ipoiji/gI';
+const sedVal = RegExp(`^(s)(\/)(.*)(\/)(.*)(\/)(?<flags>(I|p|g|w){0,4})$`);
+
+
+console.log(validateRegex(stringTest));
 
 function validateRegex(expression) {
-  if (expression !== undefined) return true;
+  let isOK = sedVal.exec(expression);
+  if (isOK !== null) {
+    if (isOK.groups.flags.length > 0) {
+      return uniqueChars(isOK.groups.flags);
+    } else if (isOK.groups.flags.length === 0) return true;
+  }
+  return false;
 }
+
+function uniqueChars(chars) {
+  for (let i = 0; i < chars.length - 1; i++) {
+    for (let b = i + 1; b <= chars.length - 1; b++) {
+      if (chars[i] === chars[b]) return false;
+    }
+  }
+  return true;
+}
+module.exports = { validateRegex };
+
+/* 
+
+ let isOK = sedVal.test(expression);
+  return isOK ? true : false ? (sedVal.gr)
+const sedExp = RegExp(`^(?<command>s)\
+  (?<openslash>\/)\
+  (?<old>.*)\
+  (?<slash>\/)\
+  (?<new>.*)\
+  (?<closeslash>\/)\
+  (?<flags>(I|p|g|w){0,4})$`);
+
+*/
