7822847a0bf8fba50f0f984bfd85054e8ad5cc78
Javier Santos <sbjaviersantos@gmail.com>
2020-05-08 03:09:06 -0600

    none

--- regexValidator.js
+++ regexValidator.js
@@ -16,25 +16,12 @@ function validateCMD(expression) {
   let valid = false;
   let isOK = expression.match(checkExp);
   if (isOK !== null) {
-    if (isOK.groups.flags.length > 0) {
-      valid = uniqueChars(isOK.groups.flags);
-    } else if (isOK.groups.file !== '' && !wFlag) valid = false;
+    if(isOK.groups.flags.length === 0) valid = true;
+    else if (isOK.groups.flags.length > 0) valid = uniqueChars(isOK.groups.flags)
+    // else if(wFlag && isOK.groups.file === ' ') valid =  false; instead we use a default bak.up file
   }
-  return valid;
+  return valid;;
 }
-
-function validateMulCMD(some) { return true};
-/*function validateMulCMD(regex) {
-  for (let reg of regex) {
-    let isOK = checkExp.exec(regex);
-    if (isOK !== null) {
-      if (isOK.groups.flags.length > 0) {
-        if (!uniqueChars(isOK.groups.flags)) return false;
-      }
-    } else return false;
-  }
-  return true;
-} */
 
 function uniqueChars(chars) {
   for (let i = 0; i < chars.length - 1; i++) {
@@ -45,4 +32,4 @@ function uniqueChars(chars) {
   }
   return true;
 }
-module.exports = { validateCMD, validateMulCMD, groupsExp };
+module.exports = { validateCMD, groupsExp };
