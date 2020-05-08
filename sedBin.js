bdf6015d3b02447f5e181aa0c9fce3f0af5886e3
Javier Santos <sbjaviersantos@gmail.com>
2020-05-07 19:55:11 -0600

    Update, using new modules functions

--- sedBin.js
+++ sedBin.js
@@ -1,31 +1,24 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
 const fs = require('fs');
+const rl = require('readline');
+const fileVal = require('./fileValidator');
+const regexVal = require('./regexValidator');
+const cmd = require('./cmd');
 
 const argv = yargs
   .usage('Usage: $0 [options] <cmd>')
-  .command('$0 <regex> <path..>', '', (yargs) => {
-    yargs
-      .positional('regex', {
-        describe: 'Regular expression to evaluate',
-        type: 'string',
-        conflicts: 'e',
-      })
-      .positional('path', {
-        describe: 'File to be processed',
-        type: 'string',
-      });
-  })
   .options({
     expression: {
       describe: 'Regex to be executed',
-      array: true,
+      array: false,
       alias: 'e',
       demandOption: false,
       nargs: 1,
       type: 'string',
+      conflicts: 'f',
     },
-    quiet: {
+    silent: {
       describe: 'Flush the output of the match',
       array: false,
       alias: ['quiet', 'n'],
@@ -34,7 +27,7 @@ const argv = yargs
       type: 'boolean',
       default: false,
     },
-    inplace: {
+    inPlace: {
       describe: 'Substitution in place, for a backup a [suffix] is necessary',
       array: false,
       alias: 'i',
@@ -44,15 +37,87 @@ const argv = yargs
       default: false,
     },
     file: {
-      describe: 'You can specified a file path commands',
-      array: true,
+      describe: 'You can specified a file with commands',
+      array: false,
       alias: 'f',
       demandCommand: false,
-      nargs: 1,
       type: 'string',
+      conflicts: 'e',
     },
   })
-  // . check to be implemented
-  .demandCommand(1, 2).argv;
+  .command('$0 <regex> <path>', '***IMPORTANTE , -f <archivo> overrides typed commands', (yargs) => {
+    yargs
+      .positional('regex', {
+        describe: 'Regular expression to evaluate',
+        type: 'string',
+        array: false
+      })
+      .positional('path', {
+        describe: 'File to be processed',
+        type: 'string',
+        array: false,
+      });
+  })
+  .check((yargs) => {
+    if (fileVal.checkReadExist(yargs.path)) {
+      return true;
+    }
+    throw new Error('Some files seems to be wrong, give them a check ;) ');
+  })
+  .check((yargs) => {
+    if (regexVal.validateCMD(yargs.regex)) {
+      return true;
+    }
+    throw new Error('Some regex seems to be wrong, give them a check ;) ');
+  })
+  .check((yargs) => {
+    if (yargs.e !== undefined) {
+      let arr = [...yargs.e];
+      if (regexVal.validateMulCMD(arr)) {
+        return true;
+      }
+      throw new Error(
+        'Some regex with -e seems to be wrong, give them a check ;) '
+      );
+    }
+    return true;
+  })
+  .check((yargs) => {
+    if (yargs.f === undefined) {
+      return true;
+    } else {
+      if (fileVal.checkReadExist(yargs.f)) {
+        return true;
+      }
+      throw new Error(
+        'The file with commands seems to be wrong, give it a check ;) '
+      );
+    }
+  })
+  .demandCommand(1, 1).argv;
 
-console.log(argv);
+let flags = {
+  n: argv.silent,
+  i: argv.inPlace,
+  e: argv.expression,
+  f: argv.file
+}
+
+if (argv.file !== undefined) {
+  let commandsFF = [];
+  let lineReader = rl.createInterface({
+    input: fs.createReadStream(argv.f),
+  });
+  lineReader.on('line', function (line) {
+    commandsFF.push(line.toString());
+  });
+  lineReader.on('close', () => {
+    cmd.executeExpression(commandsFF, argv.path,flags);
+  });
+} else if (argv.expression !== undefined) {
+  let arrayRegex = [...argv.e];
+  cmd.executeExpression(arrayRegex, argv.path, flags);
+} else {
+  let arrayRegex = [argv.regex];
+  cmd.executeExpression(arrayRegex, argv.path, flags);
+}
